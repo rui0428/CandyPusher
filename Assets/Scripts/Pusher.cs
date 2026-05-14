05e99ca924a4a6bae2c5ce7baca4260c62a01699
rui0428 <kurorui0428@icloud.com>
2026-05-14 11:48:03 +0900

    [Add] Physics to the candy
    飴のオブジェクトに物理挙動を追加した

--- Assets/Scripts/Pusher.cs
+++ Assets/Scripts/Pusher.cs
@@ -16,6 +16,13 @@ public class Pusher : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Z軸の往復移動
+        float z = Mathf.Sin(Time.time * speed) * movePower;
         
+        //自身のローカル座標の位置を最初の位置情報にｚ(Sin波の変動値)を加算して返す
+        // this.transform.localPosition 
+        //↑このコードがアタッチ(入っている)されているオブジェクトのローカル座標の情報
+        this.transform.localPosition = startPosition + new Vector3(0, 0, z);
+
     }
 }
